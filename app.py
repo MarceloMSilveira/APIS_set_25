5d8636feb5172ebca33f27ba63578b92b9b29ede
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2025-09-16 05:01:53 -0300

    set routes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, abort
 from config_db import db
 from flask_migrate import Migrate
 
@@ -16,3 +16,29 @@ from models import Plant
 with app.app_context():
     db.create_all()
 
+@app.route("/plants", methods=['GET','POST'])
+def get_plants():
+    page = request.args.get("page",1,type=int)
+    start = (page-1)*5
+    end = start + 5
+
+    plants = db.execute(db.select(Plant)).scalars().all()
+
+    formatted_plants = [plant.format() for plant in plants]
+
+    return {
+        "success": True,
+        "plants": formatted_plants[start:end],
+        "total_plants": len(formatted_plants)
+    }
+
+@app.route("/plants/<int:plant_id>")
+def get_specific_plant(plant_id):
+    try:
+        plant = db.get_or_404(Plant,plant_id)
+        return {
+            "success":True,
+            "plant":plant.format()
+        }
+    except Exception as e:
+        abort(404)
